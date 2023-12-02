11070821812e6bc7ddd7ef32de797c673515ea98
SebastianBachAndersen <ostehandler@hotmail.dk>
2023-12-02 17:02:19 +0100

    working crd

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using ProjectManagementApi.Models;
 
 namespace ProjectManagementApi.Controllers;
 
-[Route("products")]
+[Route("api/[controller]")]
+[ApiController]
 public class ProductController : Controller
 {
     private readonly ProductContext _db;
@@ -17,23 +19,37 @@ public class ProductController : Controller
     
     
     [HttpGet]
-    [Route("")]
-    public IActionResult Get()
+    public async Task<ActionResult<IEnumerable<ProductDto>>> All()
     {
-        var product = _db.Products.First();
-        
-        return Json(product.ToString());
+        return await _db.Products.Select((x) => ItemToDto(x)).ToListAsync();
     }
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ProductDto>> Get(int id)
+    {
+        var product = await _db.Products.FindAsync(id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return ItemToDto(product);
+    }
     
     [HttpPost]
-    [Route("")]
     public async Task<ActionResult<Product>> Store(Product product)
     {
         _db.Products.Add(product);
         await _db.SaveChangesAsync();
 
         return CreatedAtAction(nameof(Get), new { id = product.Id}, product);
-
     }
+
+    private static ProductDto ItemToDto(Product product) => new ProductDto
+    {
+        Id = product.Id,
+        Name = product.Name,
+        Description = product.Description,
+    };
 }
