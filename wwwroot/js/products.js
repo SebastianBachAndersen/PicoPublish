332369c094239caa194bfca38fc4c908aa9c13d6
SebastianBachAndersen <ostehandler@hotmail.dk>
2023-12-02 18:50:48 +0100

    frontend

---
+++ wwwroot/js/products.js
@@ -0,0 +1,77 @@
+﻿const path = 'api/product';
+let offset = 0;
+let pages = 10;
+
+function getItems(status = 'current') {
+    if (status === 'next') {
+        offset = offset + pages;
+    }
+    if (status === 'prev') {
+        if (offset === 0) {
+            return;
+        }
+        offset = offset - 10;
+    }
+    
+    fetch(path + `?offset=${offset}&pages=${pages}`)
+        .then((response) => response.json())
+        .then((items) => displayProducts(items)) 
+    
+}
+
+
+function displayProducts(items) {
+    console.log(items)
+    const table = document.getElementById('products') 
+    table.innerHTML = '';
+    
+    items.forEach((item) => {
+        let id = document.createElement('p');
+        id.appendChild(document.createTextNode(item.id))
+
+
+        let name = document.createElement('p');
+        name.appendChild(document.createTextNode(item.name))
+
+
+        let description= document.createElement('p');
+        description.appendChild(document.createTextNode(item.description))
+        
+        let tr = table.insertRow();
+
+        let td1 = tr.insertCell(0);
+        td1.appendChild(id);
+
+        let td2 = tr.insertCell(1);
+        let textNode = document.createTextNode(item.name);
+        td2.appendChild(name);
+
+        let td3 = tr.insertCell(2);
+        td3.appendChild(description);
+
+    })
+}
+
+function insertProduct() {
+    const name = document.getElementById('name')
+    const description = document.getElementById('description')
+    
+    fetch(path, {
+        method: "post",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({
+            name: name.value.trim(),
+            description: description.value.trim()
+        }),
+    })
+        .then((response) => response.json())
+        .then((res) => {
+            console.log(res)
+            name.value = '';
+            description.value = '';
+        })
+    
+}
